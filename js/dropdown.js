d976967f62db08fdd8d4bfe5c7e6c5c54ebe33d6
Stuart Wilding <swilding@digitalpulp.com>
2014-06-23 17:37:10 -0400

    options added

--- js/dropdown.js
+++ js/dropdown.js
@@ -5,7 +5,10 @@ var dropdownMenu = function(options){
   var self = this;
 
   defaults = {
-    menu : ''
+    menu : '',
+    show_class : 'is-visible',
+    open_delay : 200,
+    close_delay : 500
   };
 
   options = $.extend(defaults, options);
@@ -19,46 +22,44 @@ var dropdownMenu = function(options){
     self.$active_li = null;
     self.menu_active = false;
     self.menu_open = false;  
-    self.TO_enter;
-    self.TO_leave;
+    self.TO_enter = null;
+    self.TO_leave = null;
 
     self.$menu.on('mouseenter', function() {
       clearTimeout(self.TO_leave);
       self.TO_enter = window.setTimeout(function() {
         self.menu_active = true;
         if (!self.menu_open) {
-          self.$active_li.find('> ul').addClass('is-visible');
+          self.$active_li.find('> ul').addClass(options.show_class);
           self.menu_open = true;
         }
-      }, 500);
+      }, options.open_delay);
     });
 
     self.$menu.on('mouseleave', function() {
       clearTimeout(self.TO_enter);
       if (self.menu_open) {
         self.TO_leave = window.setTimeout(function() {
-          self.$active_li.find('> ul').removeClass('is-visible');
+          self.$active_li.find('> ul').removeClass(options.show_class);
           self.$active_li = null;
           self.menu_open = false;
           self.menu_active = false;
-        }, 500);
+        }, options.close_delay);
       }
     });
 
     self.$menu.on('mouseenter', '> li', function(e) {
       if (self.$active_li !== null) {
-        self.$active_li.find('> ul').removeClass('is-visible');
+        self.$active_li.find('> ul').removeClass(options.show_class);
       }
       self.$active_li = $(this);
       if (self.menu_active) {
+        self.$active_li.find('> ul').addClass(options.show_class);
         self.menu_open = true;
-        self.$active_li.find('> ul').addClass('is-visible');
       }
-
     });
   }
 
-  
   if (options.menu !== '' ) {
     self.init();
   }
